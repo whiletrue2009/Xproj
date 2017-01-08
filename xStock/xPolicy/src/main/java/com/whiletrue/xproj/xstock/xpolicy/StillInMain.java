7536ffb484de49f73619939aacb2915dcf266074
whiletrue <whiletrue@foxmail.com>
2017-01-08 22:46:40 +0800

    xpolicy init

---
+++ xStock/xPolicy/src/main/java/com/whiletrue/xproj/xstock/xpolicy/StillInMain.java
@@ -0,0 +1,16 @@
+package com.whiletrue.xproj.xstock.xpolicy;
+
+/**
+ * 使用2天及2天以上的数据，找到那些进了lhb，还未出局的yyb
+ * 
+ * @author whiletrue
+ *
+ */
+public class StillInMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
